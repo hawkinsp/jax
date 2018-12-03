11003427b18ff75ff5849547a7fe6636c0ab147c
Matthew Johnson <mattjj@google.com>
2018-12-03 15:32:47 -0800

    tweak

--- build/build_wheel_docker_entrypoint.sh
+++ build/build_wheel_docker_entrypoint.sh
@@ -2,10 +2,13 @@
 git clone -b binary-distros https://github.com/google/jax /build/jax
 cd /build/jax
 
-if [[ $1 == "cuda" ]]
+if [[ $1 == "cuda-included" ]]
 then
   python build.py --enable_cuda --cudnn_path /usr/lib/x86_64-linux-gnu/
   python build/include_cuda.py
+elif [[ $1 == "cuda" ]]
+then
+  python build.py --enable_cuda --cudnn_path /usr/lib/x86_64-linux-gnu/
 else
   python build.py
 fi
