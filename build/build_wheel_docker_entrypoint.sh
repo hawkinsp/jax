add357d189899b084d3cd99e616e9b7079fb4a8c
Matthew Johnson <mattjj@google.com>
2018-12-03 16:57:09 -0800

    update wheel build script to handle pthon3

--- build/build_wheel_docker_entrypoint.sh
+++ build/build_wheel_docker_entrypoint.sh
@@ -2,16 +2,42 @@
 git clone -b binary-distros https://github.com/google/jax /build/jax
 cd /build/jax
 
-if [[ $1 == "cuda-included" ]]
+usage() {
+  echo "usage: ${0##*/} [python2|python3] [cuda-included|cuda|nocuda]"
+  exit 1
+}
+
+if [[ $# != 3 ]]
 then
-  python build.py --enable_cuda --cudnn_path /usr/lib/x86_64-linux-gnu/
-  python build/include_cuda.py
-elif [[ $1 == "cuda" ]]
-then
-  python build.py --enable_cuda --cudnn_path /usr/lib/x86_64-linux-gnu/
-else
-  python build.py
+  usage
 fi
+python_verison_flag=$1
+cuda_flag=$2
+
+case "$python_version_flag" in
+  python3)
+    update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+    ;;
+  python2)
+    ;;
+  *)
+    usage
+esac
+
+case "$cuda_flag" in
+  cuda-included)
+    python build.py --enable_cuda --cudnn_path /usr/lib/x86_64-linux-gnu/
+    python build/include_cuda.py
+    ;;
+  cuda)
+    python build.py --enable_cuda --cudnn_path /usr/lib/x86_64-linux-gnu/
+    ;;
+  nocuda)
+    python build.py
+    ;;
+  *)
+    usage
+esac
 
 python setup.py bdist_wheel
 cp dist/*.whl /wheels/
